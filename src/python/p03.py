1631d20790b2fbabd49c3653b0a5b0a4b8f72998
GustavoAndreyIF <137062464+GustavoAndreyIF@users.noreply.github.com>
2024-08-01 16:55:18 -0300

    Update p03.py

--- src/python/p03.py
+++ src/python/p03.py
@@ -12,3 +12,38 @@ class Estudante:
     matricula: str
     data_nascimento: Data
 
+def mostrar_menu():
+    menu = """
+    ====================
+    1. Cadastrar novo estudante
+    2. Listar estudantes
+    3. Sair
+    ====================
+    """
+    print(menu)
+
+def opcao_invalida():
+    print("Opção inválida, por favor selecione uma opção válida.")
+
+mostrar_menu()
+opcao = int(input())
+existe = 0
+
+while True:
+        if opcao == 1:
+            nome = str(input("Nome: "))
+            matricula = int(input("Matricula: "))
+            data = str(input("data de nascimento: "))
+            cadastro1 = Estudante(nome, matricula, data)
+            existe = 1
+            mostrar_menu()
+            opcao = int(input())
+        if opcao == 2:
+            print(cadastro1) if existe == 1 else None
+            mostrar_menu()
+            opcao = int(input())
+        if opcao == 3:
+            break
+        if opcao != 1 and opcao != 2:
+            opcao_invalida()
+        mostrar_menu()
